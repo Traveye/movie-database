a8489d8c0bf28a552d0815981943711aff3a35bf
Big Trees <traveye@protonmail.com>
2023-02-01 12:45:39 -0800

    populating schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS movies_db;
+CREATE DATABASE movies_db;
+
+USE movies_db;
+
+DROP TABLE IF EXISTS movies;
+CREATE TABLE movies (
+    id INT PRIMARY KEY,
+    movie_name VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+    id INT,
+    movie_id INT,
+    review TEXT,
+    FOREIGN KEY(movie_id)
+    REFERENCES movies (id)
+    ON DELETE SET NULL
+);
