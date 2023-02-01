586d8c3ce0d1e53b909025807eb74ded22fe1ebc
Big Trees <traveye@protonmail.com>
2023-02-01 12:52:55 -0800

    populating seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE movies_db;
+
+INSERT INTO movies (id, movie_name)
+VALUES (1, Mean Girls),
+       (2, Shrek);
+
+INSERT INTO reviews (id, movie_id, review)
+VALUES (1, 1, "A hilarious take on the millienial highschool experience"),
+       (2, 2, "Hilarious and irreverant"),
+       (3, 2, "Trash humor");
